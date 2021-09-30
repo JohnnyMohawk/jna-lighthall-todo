dd9eac466c457f0d4376c9c178d41ce060922202
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-09-30 13:53:16 -0500

    switch from inline styling to css file

--- src/App.js
+++ src/App.js
@@ -46,22 +46,22 @@ const App = () => {
   }
 
   return (
-    <div style={styles.container}>
+    <div className="container">
       <AmplifySignOut />
       <h2>JNA Lighthall Todos</h2>
       <input
         onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
+        // style={styles.input}
         value={formState.name}
         placeholder="Name"
       />
       <input
         onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
+        // style={styles.input}
         value={formState.description}
         placeholder="Description"
       />
-      <select name="status" id="status" style={styles.input} onChange={event => setInput('status', event.target.value)}>
+      <select name="status" id="status" className="statusDrop" onChange={event => setInput('status', event.target.value)}>
           <option defaultValue="Not Started" value="Not Started">Not Started</option>
           <option value="In Progress">In Progress</option>
           <option value="Complete">Complete</option>
@@ -77,14 +77,14 @@ const App = () => {
         }} 
         placeholderText="Pick a Date"
       />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
+      <button onClick={addTodo}>Create Todo</button>
       {
         todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-            <p style={styles.todoDescription}>{todo.status}</p>
-            <p style={styles.todoDescription}>{todo.dueDate}</p>
+          <div key={todo.id ? todo.id : index} className="todo">
+            <p className="todoName">{todo.name}</p>
+            <p className="todoDescription">{todo.description}</p>
+            <p className="todoDescription">{todo.status}</p>
+            <p className="todoDescription">{todo.dueDate}</p>
           </div>
         ))
       }
@@ -92,13 +92,60 @@ const App = () => {
   )
 }
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
-}
+//   return (
+//     <div style={styles.container}>
+//       <AmplifySignOut />
+//       <h2>JNA Lighthall Todos</h2>
+//       <input
+//         onChange={event => setInput('name', event.target.value)}
+//         style={styles.input}
+//         value={formState.name}
+//         placeholder="Name"
+//       />
+//       <input
+//         onChange={event => setInput('description', event.target.value)}
+//         style={styles.input}
+//         value={formState.description}
+//         placeholder="Description"
+//       />
+//       <select name="status" id="status" style={styles.input} onChange={event => setInput('status', event.target.value)}>
+//           <option defaultValue="Not Started" value="Not Started">Not Started</option>
+//           <option value="In Progress">In Progress</option>
+//           <option value="Complete">Complete</option>
+//           <option value="Transferred">Transferred</option>
+//         </select>
+//       <DatePicker 
+//         dateFormat="MM-dd-yyyy"
+//         selected={formState.dueDate ? new Date(formState.dueDate) : undefined} 
+//         minDate={new Date()}
+//         onChange={date => {
+//           let formatDate = date.toDateString()
+//           setInput('dueDate', formatDate)
+//         }} 
+//         placeholderText="Pick a Date"
+//       />
+//       <button style={styles.button} onClick={addTodo}>Create Todo</button>
+//       {
+//         todos.map((todo, index) => (
+//           <div key={todo.id ? todo.id : index} style={styles.todo}>
+//             <p style={styles.todoName}>{todo.name}</p>
+//             <p style={styles.todoDescription}>{todo.description}</p>
+//             <p style={styles.todoDescription}>{todo.status}</p>
+//             <p style={styles.todoDescription}>{todo.dueDate}</p>
+//           </div>
+//         ))
+//       }
+//     </div>
+//   )
+// }
+
+// const styles = {
+//   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+//   todo: {  marginBottom: 15 },
+//   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+//   todoName: { fontSize: 20, fontWeight: 'bold' },
+//   todoDescription: { marginBottom: 0 },
+//   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+// }
 
 export default withAuthenticator(App)
